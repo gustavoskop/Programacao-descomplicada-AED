9cc010e2d141bf1e06a2e50668dc87e08d1a852e
gustavoskop <gkopereck@gmail.com>
2025-05-02 16:57:07 -0300

    alocacao

---
+++ StringSemVogal.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void AlocarString(char **frase, int tamanho);
+
+int main()
+{
+
+    int n, i, j = 0;
+    char *str, c;
+
+    printf("digite o tamanho da string: ");
+    scanf("%d", &n);
+
+    AlocarString(&str, n);
+
+    printf("digite a frase: ");
+    scanf(" %[^\n]", str);
+    getchar();
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        c = tolower(str[i]);
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
+            str[j++] = str[i];
+    }
+    str[j] = '\0';
+
+    printf("\nstring sem as vogais: %s", str);
+
+    free(str);
+}
+
+void AlocarString(char **frase, int tamanho)
+{
+
+    *frase = (char *)malloc((tamanho + 1) * sizeof(char));
+}
